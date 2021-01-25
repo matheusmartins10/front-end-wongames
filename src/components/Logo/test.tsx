eeae9b253d3c0fce941f9149052ea29b99b963e8
Matheus Martins <mmartins.oliveira@outlook.com>
2021-01-25 20:05:16 -0300

    Add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -11,10 +11,17 @@ describe('<Logo />', () => {
     })
   })
 
-  it('Should render a black label when passed', () => {
-    renderWithTheme(<Logo color="black" />)
+  it('Should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
-      color: '#030517'
+      width: '11rem'
+    })
+  })
+
+  it('Should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
