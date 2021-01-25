dad2fdecee69f2aecb37e6b0a862a8ab5bb67104
Matheus Martins <mmartins.oliveira@outlook.com>
2021-01-25 19:54:11 -0300

    Add color tests to Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,7 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('Should render a white label by default', () => {})
+  it('Should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      color: '#fafafa'
+    })
+  })
+
+  it('Should render a black label when passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
 })
