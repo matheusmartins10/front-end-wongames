34ea4abd099629b79af72aa4431ae1b6a4789e74
Matheus Martins <mmartins.oliveira@outlook.com>
2021-01-26 09:08:42 -0300

    Modified border at lineBottom

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -18,21 +18,19 @@ const wrapperModifiers = {
       bottom: -1rem;
       content: '';
       width: 5rem;
-      border: 0.4rem solid ${theme.colors.primary};
+      border: 0.5rem solid ${theme.colors.primary};
     }
   `
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom }) => css`
-     color: ${theme.colors[color!]}
-     font-size: ${theme.font.sizes.xlarge}
-
-     ${media.greaterThan('medium')`
-       font-size: ${theme.font.sizes.xxlarge}
-     `}
-
-     ${lineLeft && wrapperModifiers.lineLeft(theme)}
-     ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
   `}
 `
