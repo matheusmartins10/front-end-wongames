bc8d116dc1904bbe7eb3dda35d1da921043ab11c
Matheus Martins <mmartins.oliveira@outlook.com>
2021-01-27 09:35:37 -0300

    Add component MediaMatch

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,30 +1,26 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-
 import 'jest-styled-components'
 
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
+  it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
-
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      color: '#fafafa'
+      color: '#FAFAFA'
     })
   })
 
-  it('should render the heading with color black is passed', () => {
+  it('should render a black heading when color is passed', () => {
     renderWithTheme(<Heading color="black">Won Games</Heading>)
-
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       color: '#030517'
     })
   })
 
-  it('should render the heading with a line to the side left', () => {
+  it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
-
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       'border-left': '0.7rem solid #3CD3C1'
     })
