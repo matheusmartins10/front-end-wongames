9a4a45c7379c3c78ae831db719cf73c8f6791b84
Matheus Martins <mmartins.oliveira@outlook.com>
2021-02-17 22:44:54 -0300

    feat: Add GameCard structure and initial tests

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,13 +1,40 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
+const props = {
+  title: 'GameCard',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+
 describe('<GameCard />', () => {
   it('should render the heading', () => {
-    const { container } = render(<GameCard />)
+    renderWithTheme(<GameCard {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /GameCard/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /Rockstar Games/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the img', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      'https://source.unsplash.com/user/willianjusten/300x140'
+    )
+  })
+
+  it('should render the price', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
