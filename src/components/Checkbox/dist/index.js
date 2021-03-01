71511fa6d19b2d6dd5eb5f37a8bceb3082383f86
Matheus Martins <mathews_2010@outlook.com>
2021-03-01 05:26:02 -0300

    feat: Add component checkbox

---
+++ src/components/Checkbox/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var S = require("./styles");
+var Checkbox = function () { return (React.createElement(S.Wrapper, null,
+    React.createElement("input", { id: "action", type: "checkbox" }),
+    React.createElement("label", { htmlFor: "action" }, " Action "))); };
+exports["default"] = Checkbox;
