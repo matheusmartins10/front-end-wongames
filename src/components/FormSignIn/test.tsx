7d6e248354015939d4d9b1192b713bcb8fc1c45f
matheus <mmartins.oliveira@outlook.com>
2021-04-22 07:25:04 -0300

    feat: add component FormSign

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+import TextField from 'components/TextField'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+    // verifique email
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    // verifique password
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    // verifique button
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    // text
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
+
+    // link
+    expect(
+      screen.getByRole('link', {
+        name: /forgot your password\?/i
+      })
+    )
+  })
+})
