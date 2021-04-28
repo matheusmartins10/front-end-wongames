d2348a6e2524ae8b05d62cb4f7dc8085c1cbe790
matheus <mmartins.oliveira@outlook.com>
2021-04-28 19:08:38 -0300

    feat: add component formSignUp

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,89 +1,89 @@
-import { screen } from "@testing-library/react"
-import { renderWithTheme } from "utils/tests/helpers"
-import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart"
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
-import Button from "."
+import Button from '.'
 
-describe("<Button />", () => {
-  it("should render the medium size by default", () => {
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
     const { container } = renderWithTheme(<Button>Buy now</Button>)
 
-    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
-      height: "4rem",
-      padding: "0.8rem 3.2rem",
-      "font-size": "1.4rem"
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
     })
 
     expect(container.firstChild).toMatchSnapshot()
   })
 
-  it("should render the small size", () => {
+  it('should render the small size', () => {
     renderWithTheme(<Button size="small">Buy now</Button>)
 
-    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
-      height: "3rem",
-      "font-size": "1.2rem"
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
     })
   })
 
-  it("should render the large size", () => {
+  it('should render the large size', () => {
     renderWithTheme(<Button size="large">Buy now</Button>)
 
-    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
-      height: "5rem",
-      "font-size": "1.6rem",
-      padding: "0.8rem 4.8rem"
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem'
     })
   })
 
-  it("should render a fullWidth version", () => {
+  it('should render a fullWidth version', () => {
     renderWithTheme(<Button fullWidth>Buy now</Button>)
 
-    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
-      width: "100%"
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%'
     })
   })
 
-  it("should render an icon version", () => {
+  it('should render an icon version', () => {
     renderWithTheme(
       <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
     )
 
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
-    expect(screen.getByTestId("icon")).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 
-  it("should render a minimal version", () => {
+  it('should render a minimal version', () => {
     renderWithTheme(
       <Button icon={<AddShoppingCart data-testid="icon" />} minimal>
         Buy now
       </Button>
     )
 
-    expect(screen.getByRole("button", { name: /buy now/i })).toHaveStyle({
-      background: "none",
-      color: "#F231A5"
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      background: 'none',
+      color: '#F231A5'
     })
 
-    expect(screen.getByRole("button", { name: /buy now/i })).toHaveStyleRule(
-      "background",
-      "none",
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'background',
+      'none',
       {
-        modifier: ":hover"
+        modifier: ':hover'
       }
     )
   })
 
-  it("should render Button as a link", () => {
+  it('should render Button as a link', () => {
     renderWithTheme(
       <Button as="a" href="/link">
         Buy now
       </Button>
     )
 
-    expect(screen.getByRole("link", { name: /buy now/i })).toHaveAttribute(
-      "href",
-      "/link"
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
     )
   })
 })
