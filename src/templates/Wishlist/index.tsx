d07fdc1e0334d5103ddac22581eef0beae5643c9
matheus <mmartins.oliveira@outlook.com>
2021-05-05 15:12:31 -0300

    feat: add new page wishlist and a new template

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+
+import Base from 'templates/Base'
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
